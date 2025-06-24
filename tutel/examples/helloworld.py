Add topk_token_sort & fix location overflow for scatter_sample_ids (#289)
* using consistent parameter initialization for fp32 & bf16

* add topk_token_sort & fix scatter_sample_ids overflow

* add scaled_softmax_inv

---------

Signed-off-by: ghostplant <12099308+ghostplant@users.noreply.github.com>

tutel/examples/helloworld.py
@@ -23,7 +23,7 @@ parser.add_argument('--model_dim', type=int, default=2048)
 parser.add_argument('--hidden_size', type=int, default=2048)
 parser.add_argument('--num_local_experts', type=int, default=2)
 parser.add_argument('--dtype', type=str, default='float32')
-parser.add_argument('--fp32_gate', default=False, action='store_true')
+parser.add_argument('--fp32_gate', default=True, action='store_true')
 parser.add_argument('--top', type=int, default=2)
 parser.add_argument('--l_aux_wt', type=float, default=0.0)
 parser.add_argument('--a2a_ffn_overlap_degree', type=int, default=1)
@@ -107,7 +107,7 @@ if args.checkpoint_path:
     else:
         print('Checkpoint not loaded: file `%s` is not found. Will train the model from start.' % checkpoint_path)
 
-optimizer = torch.optim.SGD(model.parameters(), lr=1e-5)
+optimizer = torch.optim.SGD(model.parameters(), lr=1e-4)
 
 torch.manual_seed(0)
 x = torch.tensor(torch.randn([batch_size, num_tokens, model_dim], dtype=torch.float32, device='cpu').detach().numpy(), dtype=torch.get_default_dtype(), requires_grad=False, device=device)

tutel/examples/helloworld_from_scratch.py
@@ -70,7 +70,7 @@ class CustomMoE(torch.nn.Module):
         print(f'\n>> Get Reversed TopK Index Map of Shape[E, CAPACITY] = {top_index.shape} (-1 for padded tokens):\n{top_index}')
 
         sample_outer_batch_index = sample_index // sequence_length
-        sample_inner_sequence_index = sample_index % sequence_length
+        sample_inner_sequence_index = torch.where(sample_index >= 0, sample_index % sequence_length, sample_index)
         print(f'\n>> Get Outer-batch Index Map of Shape[E, CAPACITY] = {sample_outer_batch_index.shape} (-1 for padded tokens):\n{sample_outer_batch_index}')
         print(f'\n>> Get Inner-sequence Index Map of Shape[E, CAPACITY] = {sample_inner_sequence_index.shape} (-1 for padded tokens):\n{sample_inner_sequence_index}')
 

tutel/experts/ffn.py
@@ -38,15 +38,15 @@ class FusedExpertsNetwork(torch.nn.Module):
 
     def reset_parameters(self):
         with torch.no_grad():
+            import math
+            fc1_stdv, fc2_stdv = 1. / math.sqrt(self.batched_fc1_w.size(-1)), 1. / math.sqrt(self.batched_fc2_w.size(-2))
             for i in range(self.batched_fc1_w.size(0)):
-                fc1 = torch.nn.Linear(self.model_dim, self.hidden_size, bias=self.batched_fc1_bias is not None)
-                fc2 = torch.nn.Linear(self.hidden_size, self.output_dim, bias=self.batched_fc2_bias is not None)
-                self.batched_fc1_w[i] = fc1.weight
+                self.batched_fc1_w[i] = (torch.rand([self.hidden_size, self.model_dim], dtype=torch.float32, device=self.batched_fc1_w.device) * (fc1_stdv + fc1_stdv) - fc1_stdv).to(self.batched_fc1_w.dtype)
+                self.batched_fc2_w[i] = (torch.rand([self.hidden_size, self.output_dim], dtype=torch.float32, device=self.batched_fc2_w.device) * (fc2_stdv + fc2_stdv) - fc2_stdv).to(self.batched_fc2_w.dtype)
                 if self.batched_fc1_bias is not None:
-                    self.batched_fc1_bias[i] = fc1.bias
-                self.batched_fc2_w[i] = fc2.weight.t()
+                    self.batched_fc1_bias[i].zero_()
                 if self.batched_fc2_bias is not None:
-                    self.batched_fc2_bias[i] = fc2.bias[:self.batched_fc2_bias.size(-1)]
+                    self.batched_fc2_bias[i].zero_()
 
     def extra_repr(self):
         return 'model_dim=%d, hidden_size=%d, output_dim=%d, num_experts_per_device=%d. has_fc1_bias=%s, has_fc2_bias=%s.' % (

tutel/impls/fast_dispatch.py
@@ -211,15 +211,19 @@ def get_topk_selection(critial_data):
     return critial_data[-1].topk_ids, critial_data[-1]
 
 def get_reversed_sample_ids(critial_data, return_id_type='sample_id'):
+    from tutel import ops
     expert_ids, location_ids, capacity = torch.cat(critial_data[1]).view(len(critial_data[1]), -1), torch.cat(critial_data[2]).view(len(critial_data[2]), -1), critial_data[4]
     num_samples = expert_ids.size(-1)
-    num_tokens = critial_data[0] * capacity
-    offset = expert_ids * capacity + location_ids
-    result = torch.full([num_tokens], -1, dtype=torch.int32, device=expert_ids.device)
-    if return_id_type == 'sample_id':
-        return result.scatter_(0, offset.flatten().long(), torch.arange(offset.numel(), dtype=torch.int32, device=expert_ids.device) % num_samples).view(-1, capacity)
-    elif return_id_type == 'top_id':
-        return result.scatter_(0, offset.flatten().long(), torch.arange(offset.numel(), dtype=torch.int32, device=expert_ids.device) // num_samples).view(-1, capacity)
+    out = torch.full([critial_data[0], capacity], -1, dtype=torch.int32, device=expert_ids.device)
+
+    if return_id_type == 'top_id':
+        """ offset = torch.where(location_ids < capacity, expert_ids * capacity + location_ids, critial_data[0] * capacity)
+        out = torch.full([critial_data[0] * capacity + 1], -1, dtype=torch.int32, device=expert_ids.device)
+        out = out.scatter_(0, offset.flatten().long(), torch.arange(offset.numel(), dtype=torch.int32, device=expert_ids.device) // num_samples)[:-1].view(-1, capacity) """
+
+        return ops.scatter_sample_ids(expert_ids, location_ids, out, capacity, num_samples, True)
+    elif return_id_type == 'sample_id':
+        return ops.scatter_sample_ids(expert_ids, location_ids, out, capacity, num_samples, False)
     else:
         raise Exception(f'Unrecognized return_id_type=`{return_id_type}`')
 